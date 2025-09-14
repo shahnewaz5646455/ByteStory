6fb6a485b36baae039ab697020add9190e52407b
Rubaid07 <rubaidindu@gmail.com>
2025-09-14 23:06:37 +0600

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      hi this is it 
-      <h1>this is development</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
