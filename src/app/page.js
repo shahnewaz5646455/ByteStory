ae7bb138a2bc80295e6e284bd71cc330a930d59b
shahnewaz5646455 <shahnewaz794@gmail.com>
2025-09-16 00:12:22 +0600

    add mongodb to the project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+
 import Hero from "../../components/Hero";
 
-export default function Home() {
+export default async function Home() {
+ 
+
   return (
     <>
       <Hero />
+      {/* You can render data here */}
     </>
   );
 }
