f7b5783e68c6b441e6812ec556503f4b3346dbd7
shahnewaz5646455 <shahnewaz794@gmail.com>
2025-09-17 00:22:43 +0600

    this is it

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Feature from "../../components/Feature";
+import Hero from "../../components/Hero";
+import KeyBenefits from "../../components/KeyBenefits";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Feature />
+      <KeyBenefits />
+    </>
+  );
+}
