49ba2599d1ce7d4f6fbf1a00cb436ab971607ec8
Umair505 <moinuli359@gmail.com>
2025-09-25 09:58:22 +0600

    add Skill Assessment Quiz component to BlogPage

--- src/app/learn/page.jsx
+++ src/app/learn/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import VideoGallery from "../../../components/VideoGallery";
+import SkillAssessmentQuiz from "../../../components/SkillAssessmentQuiz";
 
 function BlogPage() {
   return (
     <>
+      <SkillAssessmentQuiz/>
       <VideoGallery></VideoGallery>
     </>
   );
